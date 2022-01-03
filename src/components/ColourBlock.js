bd00bd9ff49625debe0f4b6546397a7293bae84b
Blake <blakemcd@gmail.com>
2022-01-03 20:17:30 +1100

    added BodyText and ColourBlock

---
+++ src/components/ColourBlock.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ColourBlock() {
+    return (
+        <div className="ColourBlock">
+            
+        </div>
+    )
+}
+
+export default ColourBlock
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ColourBlock from './ColourBlock'
+
+function Header() {
+    return (
+        <header>
+            <ColourBlock/>
+        </header>
+    )
+}
+
+export default Header
